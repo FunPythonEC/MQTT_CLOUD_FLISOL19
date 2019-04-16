a2e5183793162989b6d09b992abecf76b990186a
sasilva1998 <sasilva1998@gmai.com>
2019-04-16 00:22:04 -0500

    prueba del huewheel

--- codigos/NeoPixelHuewheel/ledwheel.py
+++ codigos/NeoPixelHuewheel/ledwheel.py
@@ -1,7 +1,17 @@
+import gc
+gc.collect()
+gc.mem_free()
+
 from microWebSrv import MicroWebSrv
 from machine import Pin
 from neopixel import NeoPixel
 import json
+import network
+import time
+#conexion wifi
+sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+sta_if.connect("NETLIFE-Silva", "SASM3141")
+time.sleep(5)
 
 np = NeoPixel(Pin(13), 1)
 
@@ -9,6 +19,9 @@ def _httpHandlerLEDPost(httpClient, httpResponse):
     content = httpClient.ReadRequestContent()  # Read JSON color data
     colors = json.loads(content)
     blue, green, red = [colors[k] for k in sorted(colors.keys())]
+    print("R:"+str(red))
+    print("G:"+str(green))
+    print("B:"+str(blue))
     np[0] = (green, red, blue)
     np.write()
     httpResponse.WriteResponseJSONOk()
